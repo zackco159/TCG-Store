57da1758d2cfc7c2866327cb93adeca6ff60ef3f
DouWaLa <nguyenhaianhmedia341@gmail.com>
2024-09-22 14:44:24 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -2,25 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Thiết lập thư mục chứa tệp tĩnh
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-// Trang chính
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Trang thanh toán
-app.get('/checkout', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'checkout.html'));
-});
-
-// Trang chi tiết sản phẩm
-app.get('/product-detail', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'product-detail.html'));
-});
-
-// Bắt đầu server
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log('Server đang chạy tại http://localhost:3000');
 });
